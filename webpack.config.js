12632bbf89284c6d8a585db1b5640774d41d8c9f
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-02-11 15:32:17 -0500

    ts config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
 const path = require('path');
 module.exports = {
-    entry: path.resolve(__dirname, './client/bundle.js'),
-    devServer: {
-        // contentBase: path.resolve(__dirname, 'build'),
-        // compress: true,
-        publicPath: '/build/',
-        proxy: {
-            '/': 'http://localhost:3000'
-        }
-    },
+    // TODO: change the path
+    entry: path.resolve(__dirname, './src/index.tsx'),
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+    },
+    devServer: {
+        publicPath: '/build/',
     },
     mode: process.env.NODE_ENV,
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
     module: {
         rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+            { 
+                test: /\.(t|j)sx?$/,
+                exclude: /node_modules/, 
+                use: { loader: 'ts-loader' },
             },
+            // addition - add source-map support
+            { 
+                enforce: "pre", 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "source-map-loader", 
+            }, 
             {
                 test: /\.html$/i,
                 use: {
-                    loader: 'html-loader'
-                }
+                    loader: 'html-loader',
+                },
             },
             {
                 test: /\.(s*)(a|c)ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
@@ -44,6 +45,12 @@ module.exports = {
                   },
                 ],
               },
-        ]
-    }
+        ],
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+      },
+      // addition - add source-map support
+    devtool: "source-map",
 };
