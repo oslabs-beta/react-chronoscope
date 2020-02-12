834dfa1dc1c857b5afc820eed4f7b93139a39ef1
Matt Peters <matthew.george.peters@gmail.com>
2020-02-12 11:21:54 -0500

    progress save prior to integrating chrome and ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
 const path = require('path');
+
 module.exports = {
-    // TODO: change the path
-    entry: path.resolve(__dirname, './src/index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        publicPath: '/build/',
-    },
-    mode: process.env.NODE_ENV,
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.(t|j)sx?$/,
-                exclude: /node_modules/, 
-                use: { loader: 'ts-loader' },
-            },
-            // addition - add source-map support
-            { 
-                enforce: "pre", 
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                loader: "source-map-loader", 
-            }, 
-            {
-                test: /\.html$/i,
-                use: {
-                    loader: 'html-loader',
-                },
-            },
-            {
-                test: /\.(s*)(a|c)ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                  },
-                ],
-              },
-        ],
-    },
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM",
+  // TODO: change the path
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build/',
+  },
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'], // do we have to add styling? .scss / .css
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        use: { loader: 'ts-loader' },
       },
       // addition - add source-map support
-    devtool: "source-map",
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.html$/i,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.(s*)(a|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  // addition - add source-map support
+  devtool: 'source-map',
 };
