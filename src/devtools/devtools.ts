555023145b40c7bb054d1b4813e2ec0f82b2966a
Matt Peters <matthew.george.peters@gmail.com>
2020-02-12 11:13:11 -0500

    starting devtool integration

---
+++ src/devtools/devtools.ts
@@ -0,0 +1,7 @@
+
+chrome.devtools.panels.create(
+  'React-ChronoScope', // title for the panel tab
+  '../../assets/ChronoScope.png', // you can specify here path to an icon
+  '../index.html', // html page for injecting into the tab's content
+  null // you can pass here a callback function
+);
