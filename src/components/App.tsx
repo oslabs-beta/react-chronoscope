f16e09871be2dfba4f483bde8885c48acec51003
Vinh C <53790130+Vchau511@users.noreply.github.com>
2020-02-13 17:32:02 -0500

    test components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
+import  Test from "./Test";
+
 
 export interface HelloProps { compiler: string; framework: string; };
 
 export const App = (props: HelloProps) => {
   return (
+    <div>
+     <Test/>
+
     <h1>Hello from {props.compiler} and {props.framework}!</h1>
+
+
+    </div>
     )
 }
+
