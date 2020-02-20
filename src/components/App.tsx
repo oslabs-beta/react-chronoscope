5f1eafb9cab00725446c0cc10601ee333ecd023b
J Mei <Jmei@JMei-MacBook-Pro-7.local>
2020-02-20 11:01:55 -0500

    lineGraph testing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,16 +9,30 @@ export interface HelloProps { compiler: string; framework: string; };
 
 export const App = (props: HelloProps) => {
   return (
+    // <div>
+    //   {/* <Hello /> */}
+    //   <h1>Hello from {props.compiler} and {props.framework}!</h1>
+    //   <div id='treeGraphDiv'>
+    //     <TreeGraph/>
+    //   </div>
+    //   <div id='lineGraphDiv'>
+    //     <LineGraph/>
+    //   </div>
+    //   <h2 style={{width: '10px'}}>Test Test</h2>
+    // </div>
     <div>
-      {/* <Hello /> */}
-      <h1>Hello from {props.compiler} and {props.framework}!</h1>
-      <div id='treeGraphDiv'>
-        <TreeGraph/>
-      </div>
-      <div id='lineGraphDiv'>
-        <LineGraph/>
-      </div>
-      <h2 style={{width: '10px'}}>Test Test</h2>
+    {/* <img src="ChronoScope.png"></img> */}
+    <h1 style={{textAlign : "center", fontSize: "28px"}}>React ChronoScope</h1>
+    <hr style={{border: "2px solid black"}}></hr>
+    <h2>Tree Diagram</h2>
+    <div id='treeGraphDiv' style={{backgroundColor:"whitesmoke"}}>
+     <TreeGraph/>
+    </div>
+    <hr style={{border: "2px solid black"}}></hr>
+    <div id='lineGraphDiv'>
+     <h2>TimeLine</h2>
+     <LineGraph/>
+    </div>
     </div>
     )
 }
--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,14 +1,50 @@
 import * as React from "react";
+import Timeline from "react-visjs-timeline";
 
+const options = {
+  width: "100%",
+  height: "250px",
+  stack: true,
+  showCurrentTime: false,
+  showMajorLabels: false,
+  zoomable: true,
+  start: new Number(0),
+  end: new Number(50),
+  min: new Number(0),
+  max: new Number(70),
+  type: "range",
+  selectable: true,
+  horizontalScroll : true,
+  verticalScroll: true,
+};
+const items = [
+  {
+    start: new Number(2),
+    end: new Number(2.5), // end is optional
+    content: ""
+  },
+  {
+    start: new Number(1),
+    end: new Number(2), // end is optional
+    content: "test2"
+  },
+  {start: new Number(3),
+    end: new Number(4), // end is optional
+    content: "test3"
+  }
+];
 
 
 class LineGraph extends React.Component {
     render() {
       return (
         <div id='lineGraph' style={{}}>
-
+         <Timeline
+    options={options}
+    items={items}
+  />
         <h1> Line Test Test</h1>
-  
+      
         </div>
       );
     }
