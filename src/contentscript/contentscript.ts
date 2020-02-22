3b4c0563cad281bbf9087dd2889f70377767f7f5
Vinh C <53790130+Vchau511@users.noreply.github.com>
2020-02-22 12:34:38 -0500

    refactor background script

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,21 +1,24 @@
 //Console log to make sure Content Script is running. 
 console.log('Content Script is Running');
 
-//Typescript interface for Indexable Types. 
-interface Message {
+//Typescript interface for Indexable Types. Message from App.
+interface MessageFromApp {
   data: {
     action: string;
   }
 }
 
 // listen for message from npm package
-window.addEventListener('message', (msg: Message) => {
+window.addEventListener('message', (msg: MessageFromApp) => {
   // filter the incoming msg.data
   if (msg.data.action === 'npmToContent') {
     console.log('TreeGraph from npm: ', msg.data);
     // send the message to the chrome - backgroundScript
     // Stringify object to be sent to BackgroundScript; If Object is directly sent. 
     // Background script isn't able to read the message
-    chrome.runtime.sendMessage({action: 'ContentToBackground', payload: JSON.stringify(msg.data)});
+    chrome.runtime.sendMessage({
+      action: 'ContentToBackground', 
+      payload: JSON.stringify(msg.data)
+    });
   }
 });
