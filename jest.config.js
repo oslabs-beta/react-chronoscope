09b26adb7da5870c0935542d0b5afeb70634e308
Matt Peters <matthew.george.peters@gmail.com>
2020-02-26 12:15:37 -0500

    initial enzyme & ts setup

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  setupFilesAfterEnv: ['<rootDir>/src/components/setupEnzyme.ts'],
 };
