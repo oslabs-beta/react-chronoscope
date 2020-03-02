b9597d7a4046e7128d7db4e35b2c89a81a1064e6
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-03-01 21:06:24 -0500

    refactored hooks, typescript

--- src/backgroundScript/backgroundScript.ts
+++ src/backgroundScript/backgroundScript.ts
@@ -9,6 +9,29 @@ interface MessageFromContent {
 
 //Create Variable to store tree structure data from content script; 
 let treeGraph; 
+let port = null;
+
+// // listen for message from contentScript
+// chrome.runtime.onMessage.addListener((msg: MessageFromContent) => {
+//   console.log('Parsed Data from ContentScript: ', JSON.parse(msg.payload));
+//   console.log('Port: ', port);
+//   treeGraph = JSON.parse(msg.payload);
+//   if (!port) port = chrome.runtime.connect();
+//   port.postMessage({
+//     // action: 'BackgroundToChromeDevTool',
+//     payload: treeGraph, 
+//   })
+// });
+
+// // listen for connection from the chrome dev tool;
+// chrome.runtime.onConnect.addListener(port => {
+//   //Once connected. Background Script will send message to Chrome Dev Tool.
+//   port.postMessage({
+//     // action: 'BackgroundToChromeDevTool',
+//     payload: treeGraph, 
+//   })
+// });
+
 
 // listen for message from contentScript
 chrome.runtime.onMessage.addListener((msg: MessageFromContent) => {
--- src/components/TreeGraph.tsx
+++ src/components/TreeGraph.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Tree from 'react-d3-tree';
 
 interface treeData{
@@ -26,53 +27,82 @@ interface State {
   data: treeData[];
 }
 
-class TreeGraph extends React.Component <{}, State> {
+let port;
 
-  state: State = {
-    data : treeGraphData
-  };
+const TreeGraph: React.FC = () => {
+  const [tree, setTree] = useState<treeData[]>(treeGraphData);
 
-  componentDidMount() {
+  useEffect(() => {
+    console.log('use effect');
     // open connection with background script
     const port = chrome.runtime.connect();
     // listen for a message from the background script
     port.onMessage.addListener(message => {
-        // save the new tree
+      // save the new tree
       treeGraphData = [message.payload.payload];
-      this.setState({
-        data: treeGraphData
-      });
+      setTree(treeGraphData);
       // abort connection
       port.disconnect();
     })
-  }
+  }); 
 
-  componentDidUpdate() {
-    setInterval(() => {
-      // open connection with background script
-      const port = chrome.runtime.connect();
-      // listen for a message from the background script
-      port.onMessage.addListener(message => {
-        // save the new tree
-        tempData = [message.payload.payload];
-        // reassign the treeGraph if tempData is different from previous treeGraph
-        if (JSON.stringify(tempData) !== JSON.stringify(treeGraphData)) {
-          treeGraphData = tempData;
-          this.setState({
-            data: treeGraphData
-          });
-        }
-      })
-    }, 200);
-  }
-
-  render() {
-    return (
-      <div id='treeGraph' style={{'height': '500px'}}>
-        <Tree data={this.state.data} orientation="vertical" translate={{ x: 300, y: 20}} zoom={0.45}/>
-      </div>
-    );
-  }
+  return (
+    <div id='treeGraph' style={{'height': '500px'}}>
+      <Tree data={tree} orientation="vertical" translate={{ x: 300, y: 20}} zoom={0.45}/>
+    </div>
+  );
 }
+
+// class TreeGraph extends React.Component <{}, State> {
+//   state: State = {
+//     data : treeGraphData
+//   };
+
+//   componentDidMount() {
+//     // open connection with background script
+//     const port = chrome.runtime.connect();
+//     // listen for a message from the background script
+//     port.onMessage.addListener(message => {
+//       // save the new tree
+//       treeGraphData = [message.payload.payload];
+//       this.setState({
+//         data: treeGraphData
+//       });
+//       // abort connection
+//       port.disconnect();
+//     })
+//   }
+
+//   componentDidUpdate() {
+//     // setInterval(() => {
+//       // open connection with background script
+//       // const port = chrome.runtime.connect();
+//       // listen for a message from the background script
+//       chrome.runtime.onConnect.addListener(port => {
+//         console.log('port from componentDidUpdate: ', port);
+//         port.onMessage.addListener(message => {
+//           console.log('message from componentDidUpdate: ', message);
+//           // save the new tree
+//           tempData = [message.payload.payload];
+//           // reassign the treeGraph if tempData is different from previous treeGraph
+//           if (JSON.stringify(tempData) !== JSON.stringify(treeGraphData)) {
+//             treeGraphData = tempData;
+//             this.setState({
+//               data: treeGraphData
+//             });
+//           }
+//         })
+//       })
+//     // }, 200);
+//   }
+
+//   render() {
+//     return (
+//       <div id='treeGraph' style={{'height': '500px'}}>
+//         <Tree data={this.state.data} orientation="vertical" translate={{ x: 300, y: 20}} zoom={0.45}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default TreeGraph;
