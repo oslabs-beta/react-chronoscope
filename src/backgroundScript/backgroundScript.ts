d6d6482a9445c1592a059d7a6a3bd748153f5de3
Sergiy Alariki <sergiy.alariki.21@gmail.com>
2020-03-01 22:36:21 -0500

    fixed connection issue

--- src/backgroundScript/backgroundScript.ts
+++ src/backgroundScript/backgroundScript.ts
@@ -1,50 +1,32 @@
-//Checking to make sure background script is being run
-console.log('Background Script Running');
-
-//Typescript interface for Indexable Types. Message from ContentScript.
 interface MessageFromContent {
   action: string;
   payload: string;
 }
 
-//Create Variable to store tree structure data from content script; 
+// Create Variable to store tree structure data from content script; 
 let treeGraph; 
-let port = null;
+let currentPort;
 
-// // listen for message from contentScript
-// chrome.runtime.onMessage.addListener((msg: MessageFromContent) => {
-//   console.log('Parsed Data from ContentScript: ', JSON.parse(msg.payload));
-//   console.log('Port: ', port);
-//   treeGraph = JSON.parse(msg.payload);
-//   if (!port) port = chrome.runtime.connect();
-//   port.postMessage({
-//     // action: 'BackgroundToChromeDevTool',
-//     payload: treeGraph, 
-//   })
-// });
-
-// // listen for connection from the chrome dev tool;
-// chrome.runtime.onConnect.addListener(port => {
-//   //Once connected. Background Script will send message to Chrome Dev Tool.
-//   port.postMessage({
-//     // action: 'BackgroundToChromeDevTool',
-//     payload: treeGraph, 
-//   })
-// });
-
+// listen for connection from the chrome dev tool;
+chrome.runtime.onConnect.addListener(port => {
+  //Once connected. Background Script will send message to Chrome Dev Tool.
+  currentPort = port;
+  port.postMessage({
+    action: 'BackgroundToChromeDevTool',
+    payload: treeGraph, 
+  })
+});
 
 // listen for message from contentScript
 chrome.runtime.onMessage.addListener((msg: MessageFromContent) => {
   console.log('Parsed Data from ContentScript: ', JSON.parse(msg.payload));
   treeGraph = JSON.parse(msg.payload);
+  if (currentPort) {
+    currentPort.postMessage({
+      // action: 'BackgroundToChromeDevTool',
+      payload: treeGraph, 
+    })
+  }
 });
 
-// listen for connection from the chrome dev tool;
-chrome.runtime.onConnect.addListener(port => {
-  //Once connected. Background Script will send message to Chrome Dev Tool.
-  port.postMessage({
-    // action: 'BackgroundToChromeDevTool',
-    payload: treeGraph, 
-  })
-});
 
--- src/components/TreeGraph.tsx
+++ src/components/TreeGraph.tsx
@@ -8,7 +8,6 @@ interface treeData{
 }
 
 let treeGraphData: treeData[];
-let tempData: treeData[];
 
 // initial tree data
 treeGraphData = [
@@ -23,39 +22,23 @@ treeGraphData = [
   }
 ];
 
-interface State {
-  data: treeData[];
-}
-
-// let port;
+let port;
 
 const TreeGraph: React.FC = () => {
   const [tree, setTree] = useState<treeData[]>(treeGraphData);
 
-  const port = chrome.runtime.connect();
-  // listen for a message from the background script
-  port.onMessage.addListener(message => {
-    console.log('from port on message');
-    if (JSON.stringify([message.payload.payload]) !== JSON.stringify(treeGraphData)) {
-      // save the new tree
-      treeGraphData = [message.payload.payload];
-      setTree(treeGraphData);
-    }
-  })
-
   useEffect(() => {
-    console.log('use effect');
     // open connection with background script
-    // if (!port) port = chrome.runtime.connect();
-    // const port = chrome.runtime.connect();
-    // // listen for a message from the background script
-    // port.onMessage.addListener(message => {
-    //   // save the new tree
-    //   if (JSON.stringify([message.payload.payload]) !== JSON.stringify(treeGraphData)) {
-    //     treeGraphData = [message.payload.payload];
-    //     setTree(treeGraphData);
-    //   }
-    // })
+    if (!port) port = chrome.runtime.connect();
+    port = chrome.runtime.connect();
+    // listen for a message from the background script
+    port.onMessage.addListener(message => {
+      // save the new tree
+      if (JSON.stringify([message.payload.payload]) !== JSON.stringify(treeGraphData)) {
+        treeGraphData = [message.payload.payload];
+        setTree(treeGraphData);
+      }
+    })
   }); 
 
   return (
@@ -64,57 +47,5 @@ const TreeGraph: React.FC = () => {
     </div>
   );
 }
-
-// class TreeGraph extends React.Component <{}, State> {
-//   state: State = {
-//     data : treeGraphData
-//   };
-
-//   componentDidMount() {
-//     // open connection with background script
-//     const port = chrome.runtime.connect();
-//     // listen for a message from the background script
-//     port.onMessage.addListener(message => {
-//       // save the new tree
-//       treeGraphData = [message.payload.payload];
-//       this.setState({
-//         data: treeGraphData
-//       });
-//       // abort connection
-//       port.disconnect();
-//     })
-//   }
-
-//   componentDidUpdate() {
-//     // setInterval(() => {
-//       // open connection with background script
-//       // const port = chrome.runtime.connect();
-//       // listen for a message from the background script
-//       chrome.runtime.onConnect.addListener(port => {
-//         console.log('port from componentDidUpdate: ', port);
-//         port.onMessage.addListener(message => {
-//           console.log('message from componentDidUpdate: ', message);
-//           // save the new tree
-//           tempData = [message.payload.payload];
-//           // reassign the treeGraph if tempData is different from previous treeGraph
-//           if (JSON.stringify(tempData) !== JSON.stringify(treeGraphData)) {
-//             treeGraphData = tempData;
-//             this.setState({
-//               data: treeGraphData
-//             });
-//           }
-//         })
-//       })
-//     // }, 200);
-//   }
-
-//   render() {
-//     return (
-//       <div id='treeGraph' style={{'height': '500px'}}>
-//         <Tree data={this.state.data} orientation="vertical" translate={{ x: 300, y: 20}} zoom={0.45}/>
-//       </div>
-//     );
-//   }
-// }
 
 export default TreeGraph;
